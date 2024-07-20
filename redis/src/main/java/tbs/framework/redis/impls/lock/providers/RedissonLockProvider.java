6960c70ccb6866b2526f43074e9fce3cba508029
大帅比 童 <tongjames@live.com>
2024-07-20 13:34:11 +0800

    feat: 实现RedissonLockProvider类

--- redis/src/main/java/tbs/framework/redis/impls/lock/providers/RedissonLockProvider.java
+++ redis/src/main/java/tbs/framework/redis/impls/lock/providers/RedissonLockProvider.java
@@ -7,11 +7,30 @@ import tbs.framework.redis.impls.lock.RedissonLockImpl;
 
 import javax.annotation.Resource;
 
+/**
+ * 使用Redisson客户端提供锁的实现。
+ *
+ * <p>本类实现了ILockProvider接口，用于提供Redisson锁的实例。通过注入RedissonClient，可以方便地创建和配置锁。
+ *
+ * @author Abstergo
+ * @since 1.0
+ */
 public class RedissonLockProvider implements ILockProvider {
 
+    /**
+     * Redisson客户端，用于创建和操作锁。
+     */
     @Resource
-    RedissonClient redissonClient;
+    private RedissonClient redissonClient;
 
+    /**
+     * 获取锁实例。
+     *
+     * <p>通过传入的目标对象，创建一个对应的Redisson锁实例。
+     *
+     * @param target 目标对象
+     * @return 锁实例
+     */
     @Override
     public ILock getLocker(Object target) {
         return new RedissonLockImpl(redissonClient, target);
