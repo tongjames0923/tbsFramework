d44b734a284d92539a7a43d75df98a2c56d145f7
abstergo <tongjames@live.com>
2024-08-05 21:14:14 +0800

    feat(auth): 添加缓存防抖功能

--- auth/src/main/java/tbs/framework/auth/config/DebounceConfig.java
+++ auth/src/main/java/tbs/framework/auth/config/DebounceConfig.java
@@ -1,12 +1,15 @@
 package tbs.framework.auth.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import tbs.framework.auth.interfaces.IApiInterceptor;
 import tbs.framework.auth.interfaces.debounce.IDebounce;
 import tbs.framework.auth.interfaces.debounce.impls.AESTokenDebounce;
+import tbs.framework.auth.interfaces.debounce.impls.CacheDebounce;
 import tbs.framework.auth.interfaces.debounce.impls.DebounceInterceptor;
 import tbs.framework.auth.properties.AuthProperty;
+import tbs.framework.cache.managers.AbstractExpireManager;
 
 import javax.annotation.Resource;
 
@@ -21,8 +24,15 @@ public class DebounceConfig {
     }
 
     @Bean
-    @ConditionalOnMissingBean(IDebounce.class)
+    @ConditionalOnMissingBean({IDebounce.class, AbstractExpireManager.class})
     AESTokenDebounce simpleDefaultDebounce() {
         return new AESTokenDebounce(authProperty.getApiStabilizationField(), authProperty.getApiColdDownTime());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(IDebounce.class)
+    @ConditionalOnBean(AbstractExpireManager.class)
+    CacheDebounce cacheDebounce(AbstractExpireManager expireManager) {
+        return new CacheDebounce(expireManager, authProperty.getApiColdDownTime());
     }
 }
